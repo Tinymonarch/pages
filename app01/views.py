f2ef2ba6cae159953310a0e15f7ea83c0223d115
Tintmonarch <sap_wxy@163.com>
2018-11-29 17:40:56 +0800

    20181129

--- app01/views.py
+++ app01/views.py
@@ -3,9 +3,31 @@ from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 
 USER_LIST = []
-for i in range(1,999):
+for i in range(1,201):
     temp = {'name': 'root'+str(i), 'age': i}
     USER_LIST.append(temp)
+
+
+class CustomPaginator(Paginator):
+    def __init__(self, current_page, per_page_num, *args, **kwargs):
+        self.current_page = int(current_page)    # 当前页
+        self.per_page_num = int(per_page_num)    # 每次最多显示的页码数量
+        super(CustomPaginator, self).__init__(*args, **kwargs)
+
+    def pager_num_range(self):
+        # 当前页： self.current_page
+        # 最多显示的页码数量 self.per_page_num
+        # 总页数 num_pages
+        if self.num_pages < self.per_page_num:
+            return range(1, self.num_pages+1)
+        part = int(self.per_page_num/2)
+        if self.current_page <= part:
+            return range(1, self.per_page_num+1)
+        if (self.current_page + part) > self.num_pages:
+            return range(self.num_pages - self.per_page_num, self.num_pages+1)
+
+        return range(self.current_page-part, self.current_page+part+1)
+
 
 def index(request):
     per_page_count = 10
@@ -27,15 +49,15 @@ def index(request):
 
 
 def index1(request):
-
-    paginator = Paginator(USER_LIST, 10)
+    current_page = request.GET.get('p')
+    paginator = CustomPaginator(current_page, 11, USER_LIST, 10)
     # 全部数据：    USER_LIST,=> 得出的共有多少条数据
     # per_page:    每页显示条目数量
     # count：      数据总个数
     # num_pages：  总页数
     # page_range:  总页数的索引范围，如：（1，10） （1，200）
     # page：       page对象
-    current_page = request.GET.get('p')
+
     try:
         posts = paginator.page(current_page)
         #has_next               是否有下一页
